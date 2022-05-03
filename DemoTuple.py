3271cd725cf97c5b862697f91a2e9918d1254720
papasmf1 <papasmf1@gmail.com>
2022-05-03 10:38:53 +0900

    first commit

---
+++ DemoTuple.py
@@ -0,0 +1,13 @@
+# DemoTuple.py
+#세트를 사용하는 경우
+a = {1,2,3,3}
+print(a)
+print(type(a))
+b = {3,4,4,5}
+print(b)
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
+
+
+
---
+++ demo.py
@@ -0,0 +1,4 @@
+# demo.py 
+# 파이썬에서 주석
+print("hello python")
+
