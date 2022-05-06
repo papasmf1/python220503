98a00d5c666a4f45ee351fb1823257c62dccb345
papasmf1 <papasmf1@gmail.com>
2022-05-06 14:17:10 +0900

    파일로 저장

--- web3.py
+++ web3.py
@@ -4,10 +4,12 @@ import urllib.request
 #크롤링 
 from bs4 import BeautifulSoup
 
-
-#페이지 번호 생성
+#파일로 저장 
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
+#페이지 번호 생성(페이지 처리)
 for i in range(1,6):
     url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
+    print(url)
     data = urllib.request.urlopen(url)
     soup = BeautifulSoup(data, "html.parser")
 
@@ -16,3 +18,6 @@ for i in range(1,6):
     for tag in cartoons:
         title = tag.find("a")
         print(title.text.strip())
+        f.write(title.text.strip() + "\n")
+
+f.close() 
