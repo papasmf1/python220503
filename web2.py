0cfcdcdb315d494a23abf59cab3027afc8d2d6a1
papasmf1 <papasmf1@gmail.com>
2022-05-06 14:04:46 +0900

    네이버 웹툰

--- web2.py
+++ web2.py
@@ -8,4 +8,12 @@ from bs4 import BeautifulSoup
 # <td class="title">
 # 	<a href="/webtoon/detail?">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
 # </td>
-                        
+data = urllib.request.urlopen(
+    "http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri") 
+soup = BeautifulSoup(data, "html.parser")
+
+cartoons = soup.find_all("td", class_="title")
+title = cartoons[0].find("a").text 
+link = cartoons[0].find("a")["href"]
+print(title)
+print(link)
