e9c76ba44ec2e030067d232d6d4ee915185828af
papasmf1 <papasmf1@gmail.com>
2022-05-06 09:37:39 +0900

    문자여7ㄹ 연습2

--- DemoStr.py
+++ DemoStr.py
@@ -7,3 +7,19 @@ for name in names:
     print("안녕하세요 {0}님! 이제는 더운 여름입니다.".format(name))
     print("=" * 40)
 
+#문자열 처리
+u = "<<< spam and ham >>>"
+result = u.strip("<> ")
+print(u)
+print(result)
+strA = "python is very poweful"
+print(len(strA))
+print(strA.capitalize())
+print(strA.count("p"))
+print(strA.count("p",7))
+result2 = result.replace("spam", "spam egg")
+print(result2)
+lst = result2.split()
+print(lst)
+print("---다시 원복---")
+print(":)".join(lst))
