5bf3cd46f6997c0db5e8abcb043b77f3e1b9885c
papasmf1 <papasmf1@gmail.com>
2022-05-06 13:43:19 +0900

    네이버 웹툰

---
+++ web2.py
@@ -0,0 +1,11 @@
+# web2.py 
+#웹서버에 페이지 실행 요청
+import urllib.request
+#크롤링 
+from bs4 import BeautifulSoup
+
+#블럭 주석: ctrl + / 
+# <td class="title">
+# 	<a href="/webtoon/detail?">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
+# </td>
+                        
